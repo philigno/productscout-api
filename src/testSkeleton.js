4a728037ca1b0eca51002b9e048310b274427ad9
philigno <122437800+philigno@users.noreply.github.com>
2026-01-13 23:48:16 +0100

    Delete src/testSkeleton.js

--- src/testSkeleton.js
+++
@@ -1,8 +0,0 @@
-import { testProductScoutSkeleton } from "./services/productscoutGenerator.js";
-
-async function run() {
-  const result = await testProductScoutSkeleton({ lang: "fr" });
-  console.log(result);
-}
-
-run();
